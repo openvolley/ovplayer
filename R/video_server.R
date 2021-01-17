f07d889493ab66a099af0d0ed6db562361180080
Ben Raymond <ben@theraymonds.org>
2021-01-17 15:28:22 +1100

    pass host to video_server, minor tidyup v0.0.4

--- R/video_server.R
+++ R/video_server.R
@@ -4,19 +4,22 @@
 #'
 #' @param method string: either "lighttpd" or "servr". If "lighttpd" but the lighttpd executable cannot be found, "servr" will be used as a fallback
 #' @param port integer: the port to open the server on. If missing or `NULL`, a random port between 8001 and 12000
+#' @param host string: server host IP address (defaults to "localhost")
 #'
 #' @return A list with components `method`, `url`, `port`, `dir` (the source dir from which files will be served), and `cleanup_fun` (a function to run when finished with the server, which will stop the server process and clean up its `dir`)
 #'
 #' @seealso \code{\link{ovp_shiny}}
 #'
 #' @export
-ovp_video_server <- function(method, port) {
+ovp_video_server <- function(method, port, host) {
     assert_that(is.string(method))
     method <- match.arg(tolower(method), c("lighttpd", "servr"))
+    if (missing(host) || is.null(host) || is.na(host)) host <- "localhost"
+    assert_that(is.string(host))
     if (missing(port) || is.null(port) || is.na(port)) port <- sample.int(4000, 1) + 8000L ## random port from 8001
     assert_that(is.integer(port), !is.na(port))
 
-    video_server_url <- paste0("http://localhost:", port, "/")
+    video_server_url <- paste0("http://", host, ":", port, "/")
     video_server_dir <- tempfile()
     dir.create(video_server_dir)
     if (method == "lighttpd") {
@@ -41,7 +44,7 @@ ovp_video_server <- function(method, port) {
         }
     } else {
         ## start servr instance
-        servr::httd(dir = video_server_dir, port = port)
+        servr::httd(dir = video_server_dir, port = port, host = host)
         cleanup_fun <- function() {
             message("cleaning up servr")
             servr::daemon_stop()
